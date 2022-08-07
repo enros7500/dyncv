bc9b7858d689378e0ba941653d8afd52db670de1
enros7500 <enros7500@gmail.com>
2022-08-07 14:10:29 +0200

    main

--- dyncv/app/routes/index.tsx
+++ dyncv/app/routes/index.tsx
@@ -1,32 +1,18 @@
+
+
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import Navbar from '~/components/navbar'
+import Sidebar from '~/components/sidebar';
+
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+  return ([
+      <>
+        <Navbar key="first"/>
+          <Sidebar />
+        <h1>Hey Dud</h1>
+      </>
+  ]);
 }
